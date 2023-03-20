da7b7b3ed9977180425433b637ea275755e69460
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-03-20 23:49:56 +0200

    Commit 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import styled from '@emotion/styled';
+import Home from "pages/Home/Home";
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <ul>
+        <li>
+          <StyledLink to="/">Home</StyledLink>
+          </li>
+        <li>
+          <StyledLink to="/cats">Collection</StyledLink>
+          </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cats" element={<>Collection</>} />
+        <Route path="/cats/:catId" element={<>Collection element</>} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+
+    const CatImg = '../../images/PSD-files-Cat.png';
+
+    return <>Home page
+    <img src={CatImg} alt="cat" width="370" />
+    </>
+}
+
+export default Home;
